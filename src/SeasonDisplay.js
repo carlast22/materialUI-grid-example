01d8c0d4a89520feeb43163f211ac71166c482dc
Carla Suarez <carlast22@gmail.com>
2019-11-07 11:18:56 -0500

    Trying nested grids

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Grid, makeStyles, Hidden } from "@material-ui/core";
+import { Grid, makeStyles, Hidden, Paper } from "@material-ui/core";
 
 const useStyles = makeStyles({
   header: {
@@ -16,13 +16,27 @@ const useStyles = makeStyles({
 
 const SeasonDisplay = () => {
   const classes = useStyles();
+
+
   return (
     <div>
       <div id="header"></div>
       <div id="body">
+      <Grid container spacing={4}>
+        <Grid container item xs={12} spacing={3}>
+          <Grid item xs={4} lg={7} lg-offset={5}>
+            <Paper className={classes.paper}>item1</Paper>
+          </Grid>
+          <Grid item xs={4}>
+            <Paper className={classes.paper}>item2</Paper>
+          </Grid>
+        </Grid>
+      </Grid>
         <Grid container spacing={4}>
-          <Grid item md={12} xs={12} className={classes.header}>
-            HEADER
+          <Grid container item xs={12} spacing={3}>
+            <Grid item xs={4}>
+              <Paper className={classes.paper}>item3</Paper>
+            </Grid>
           </Grid>
           {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12].map(value => (
             <Grid key={value} item md={1} xs={1}>
@@ -32,13 +46,13 @@ const SeasonDisplay = () => {
           <Hidden mdDown>
             <Grid item lg={3}>
               <div id="navBar" className={classes.navBar}>
-                face
+                NAV BAR
               </div>
             </Grid>
           </Hidden>
           <Grid item lg={9} xs={12} md={12}>
             <div id="navBar" className={classes.navBar}>
-              face
+              FORM
             </div>
           </Grid>
         </Grid>
